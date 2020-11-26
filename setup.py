f84d77f5430e544982dbdc6a16ffc3f0d3eca79c
Eli Stone <eli.stonium@gmail.com>
2020-11-25 21:14:03 -0700

    2.0-rc1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="qencoder",
-    version="1.5.6",
+    version="2.0-rc1",
     author="Eli Stone",
     author_email="eli.stonium@gmail.com",
     description="Qt graphical interface for encoding",

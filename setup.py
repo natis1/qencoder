eba7376d0943edd30d142a1474d40f064ee3687b
Eli Stone <eli.stonium@gmail.com>
2020-12-21 13:18:57 -0700

    Bump version to 2.0 for pypi official release (windows builds coming later like in a few weeks ok bugtesting on windows is hard)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="qencoder",
-    version="2.0-rc6",
+    version="2.0",
     author="Eli Stone",
     author_email="eli.stonium@gmail.com",
     description="Qt graphical interface for encoding",
